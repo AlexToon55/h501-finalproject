77073e8365d2f157a61f0bf3787c58432a646fbb
Alex Toon <altoon@IU.EDU>
2025-10-01 18:55:56 -0400

    Update streamlit_app.py

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -3,7 +3,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-page = st_navbar(["Home", "Are we Published?", "Charts", "Chart2", "Did someone say charts?"])
 st.write("Streamlit supports a wide range of data visualizations, including [Plotly, Altair, and Bokeh charts](https://docs.streamlit.io/develop/api-reference/charts). 📊 And with over 20 input widgets, you can easily make your data interactive!")
 
 all_users = ["Alice", "Bob", "Charly"]
