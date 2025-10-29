fe4f5ba9610981cbb0f00b464429f05037ce1f27
BrianJBlandino <bblandi@iu.edu>
2025-10-29 14:04:34 -0400

    attempting to change subheader

--- modules/app_core.py
+++ modules/app_core.py
@@ -17,7 +17,7 @@ def page_header(title: str):
     """ Display a standardized page header. """
     import streamlit as st
     st.header(title)
-    st.info(f"Music & Mental Health")
+    st.info(f"Music & Mental Health - {title}")
 
 
 def kpis(df):
