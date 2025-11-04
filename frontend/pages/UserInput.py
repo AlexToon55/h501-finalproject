ea70c6f2fbecfcc2a04785dc1959eb2fc71afb0b
BrianJBlandino <bblandi@iu.edu>
2025-11-04 22:51:28 +0000

    setting up user input page

---
+++ frontend/pages/UserInput.py
@@ -0,0 +1,15 @@
+# importing the libraries
+import modules.bootstrap
+from modules.app_core import config, survey, page_header
+from modules.nav import sidebar
+import streamlit as st
+import pandas as pd
+from datetime import datetime
+import numpy as np
+import plotly.express as px
+
+config("User Input") # sets the page title and icon
+sidebar() # add any extra sidebar elements here
+# df = survey() # load and cache the dataset
+page_header("Give Your Feedback")
+
