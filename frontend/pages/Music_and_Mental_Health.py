6bb8be32208d3dc87ae0243f7645f35545c58b65
BrianJBlandino <bblandi@iu.edu>
2025-12-03 19:27:49 -0500

    updating img pull methodology

--- frontend/pages/Music_and_Mental_Health.py
+++ frontend/pages/Music_and_Mental_Health.py
@@ -22,7 +22,7 @@ df = survey()
 st.title("Music and Mental Health")
 
 # setting an image
-MentalHealth = links_from_secrets("MentalHealthImage")
+st.image('frontend/assets/mental-health.jpg', width = 500)
 
 if MentalHealth:
     st.image(MentalHealth, width = 500)
